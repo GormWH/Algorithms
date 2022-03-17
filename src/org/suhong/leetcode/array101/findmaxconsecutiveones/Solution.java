326cf91ca19444ef2dfef971fbd421bbb1d0c15d
GormWH <sp4kid@gmail.com>
2022-03-17 18:36:21 +0900

    initial commit

---
+++ src/org/suhong/leetcode/array101/findmaxconsecutiveones/Solution.java
@@ -0,0 +1,28 @@
+package org.suhong.leetcode.array101.findmaxconsecutiveones;
+
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int currentMax = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                currentMax = 0;
+                continue;
+            }
+
+            currentMax++;
+            if (currentMax > max) {
+                max = currentMax;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {1,1,0,1,1,1};
+        int[] nums2 = {1,0,1,1,0,1};
+        System.out.println(solution.findMaxConsecutiveOnes(nums1));
+        System.out.println(solution.findMaxConsecutiveOnes(nums2));
+    }
+}
