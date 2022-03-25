f115c19cfc8df141a0e18ac5855589f1275e93ab
GormWH <sp4kid@gmail.com>
2022-03-25 23:28:09 +0900

    arrays101: remove duplicates from sorted array

---
+++ src/org/suhong/leetcode/array101/removeduplicatesfromsortedarray/Solution.java
@@ -0,0 +1,32 @@
+package org.suhong.leetcode.array101.removeduplicatesfromsortedarray;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int removeDuplicates(int[] nums) {
+
+        int length = nums.length;
+        int numOfInterest = nums[0];
+        int writer = 1;
+
+        for (int i = 1; i < length; i++) {
+            int current = nums[i];
+            if (current != numOfInterest) {
+                nums[writer++] = current;
+                numOfInterest = current;
+            }
+        }
+        return writer;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,1,2};
+        int[] nums2 = {0,0,1,1,1,2,2,3,3,4};
+
+        Solution solution = new Solution();
+        System.out.println(solution.removeDuplicates(nums1) + Arrays.toString(nums1));
+        System.out.println(solution.removeDuplicates(nums2) + Arrays.toString(nums2));
+
+    }
+}
