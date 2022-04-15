dc3af581cc5cdb21383d5ebc84308e811d01afb2
GormWH <sp4kid@gmail.com>
2022-04-15 23:56:43 +0900

    programmers: report bad users...not going well..TTTT

--- src/org/suhong/programmers/level1/ReportBadUsers.java
+++ src/org/suhong/programmers/level1/ReportBadUsers.java
@@ -1,17 +1,20 @@
 package org.suhong.programmers.level1;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class ReportBadUsers {
-    public int[] solution(String[] users, String[] reports, int k) {
+    public int[] solution(String[] user_ids, String[] reports, int k) {
+        Map<String, User> userMap = new HashMap<>();
+        for (int i = 0; i < user_ids.length; i++) {
+            String user_id = user_ids[i];
+            userMap.put(user_id, new User(i, user_id));
+        }
 
+        for (int i = 0; i < reports.length; i++) {
+            String[] report = reports[i].split(" ");
 
-        return answer;
+        }
     }
-
-    public
 
     public static void main(String[] args) {
         ReportBadUsers reportBadUsers = new ReportBadUsers();
@@ -22,5 +25,19 @@ public class ReportBadUsers {
 
         int[] result = reportBadUsers.solution(idList, reportList, limit);
         System.out.println(Arrays.toString(result));
+    }
+}
+
+class User {
+    int index;
+    String name;
+    List<User> reportedUsers;
+    int count;
+
+    public User(int index, String name) {
+        this.index = index;
+        this.name = name;
+        this.reportedUsers = new ArrayList<>();
+        this.count = 0;
     }
 }
