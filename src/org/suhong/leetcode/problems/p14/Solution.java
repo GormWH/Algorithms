3933e3686764c180cfb81ebc709e6a463f787233
GormWH <sp4kid@gmail.com>
2022-06-04 22:24:53 +0900

    LeetCode: Problem14 - Longest common prefix(solved) - correct solution

--- src/org/suhong/leetcode/problems/p14/Solution.java
+++ src/org/suhong/leetcode/problems/p14/Solution.java
@@ -1,7 +1,7 @@
 package org.suhong.leetcode.problems.p14;
 
 // https://leetcode.com/problems/longest-common-prefix/
-public class Solution {
+class Solution {
 
     // misunderstood the question.
     public String longestCommonPrefix1(String[] strs) {
@@ -40,13 +40,31 @@ public class Solution {
         return longestSuffix;
     }
 
+    // second shot.
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder builder = new StringBuilder();
+        String firstElement = strs[0];
+        for (int i = 0; i < firstElement.length(); i++) {
+            char currentCharacter = firstElement.charAt(i);
+            for (int j = 0; j < strs.length; j++) {
+                if (strs[j].length() <= i || strs[j].charAt(i) != currentCharacter) {
+                    return builder.toString();
+                }
+            }
+            builder.append(currentCharacter);
+        }
+        return builder.toString();
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
 
         String[] strs1 = {"flower","flow","flight"};
         String[] strs2 = {"dog","racecar","car"};
+        String[] strs3 = {"ab","a"};
 
         System.out.println(s.longestCommonPrefix(strs1));
         System.out.println(s.longestCommonPrefix(strs2));
+        System.out.println(s.longestCommonPrefix(strs3));
     }
 }
