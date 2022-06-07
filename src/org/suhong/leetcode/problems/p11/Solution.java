3efd191df89b5dc35ef946a804e99fda7e3ef8cd
GormWH <sp4kid@gmail.com>
2022-06-07 21:32:06 +0900

    LeetCode: Problem11 - Container With Most Water(solving...) - initiate settings

---
+++ src/org/suhong/leetcode/problems/p11/Solution.java
@@ -0,0 +1,19 @@
+package org.suhong.leetcode.problems.p11;
+
+// https://leetcode.com/problems/container-with-most-water/
+class Solution {
+    public int maxArea(int[] height) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] nums1 = {1,8,6,2,5,4,8,3,7};
+        int[] nums2 = {1,1};
+
+        System.out.println(s.maxArea(nums1));
+        System.out.println(s.maxArea(nums2));
+    }
+}
