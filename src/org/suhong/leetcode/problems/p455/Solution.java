7a506bf07f12ac447eb722e36a24beaa2f90834e
GormWH <sp4kid@gmail.com>
2022-06-08 23:20:51 +0900

    LeetCode: Problem455 - assign cookies(solving...) - initial settings

---
+++ src/org/suhong/leetcode/problems/p455/Solution.java
@@ -0,0 +1,15 @@
+package org.suhong.leetcode.problems.p455;
+
+// https://leetcode.com/problems/assign-cookies/
+public class Solution {
+
+    public int findContentChildren(int[] g, int[] s) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[][] gs1 = {{1,2,3},{1,1}};
+        int[][] gs2 = {{1,2},{1,2,3}};
+    }
+}
