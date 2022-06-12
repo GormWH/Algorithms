fe1638ad72fddf43bdcd530243a250f0e8a03f78
GormWH <sp4kid@gmail.com>
2022-06-12 15:14:13 +0900

    LeetCode - p15:Three Sum(solved) - almost brute force

--- src/org/suhong/leetcode/problems/p15/Solution.java
+++ src/org/suhong/leetcode/problems/p15/Solution.java
@@ -5,37 +5,36 @@ import java.util.stream.Collectors;
 
 // https://leetcode.com/problems/3sum/
 class Solution {
+    // almost brute force
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            result.addAll(twoSum(nums, i));
-        }
-        return result;
-    }
 
-    private List<List<Integer>> twoSum(int[] nums, int fistIdx) {
-        List<List<Integer>> result = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        int target = -nums[fistIdx];
-        for (int i = 0; i < nums.length; i++) {
-            if (i == fistIdx) continue;
-            map.put(target - nums[i], i);
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            Integer current = nums[i];
-            Integer search = map.get(current);
-            if (search != null && search != i && search != fistIdx) {
-                List<Integer> answer = new ArrayList<>();
-                answer.add(-target);
-                answer.add(target - current);
-                answer.add(current);
-                result.add(answer);
+        Arrays.sort(nums); // first, sort array(to avoid duplicates)
+        Integer firstPrev = null;
+        for (int i = 0; i < nums.length - 2; i ++) {
+            Integer firstNum = nums[i];
+            if (firstNum.equals(firstPrev)) continue;
+            else firstPrev = firstNum;
+            Integer secondPrev = null;
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                Integer secondNum = nums[j];
+                if (secondNum.equals(secondPrev)) continue;
+                else secondPrev = secondNum;
+                int thirdIdx = Arrays.binarySearch(nums, j+1, nums.length, -firstNum - secondNum);
+                if (thirdIdx >= 0) {
+                    List<Integer> triplet = new ArrayList<>();
+                    triplet.add(firstNum);
+                    triplet.add(secondNum);
+                    triplet.add(nums[thirdIdx]);
+                    result.add(triplet);
+                }
             }
         }
+
         return result;
     }
+
+
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] nums1 = {-1,0,1,2,-1,-4};
@@ -50,7 +49,7 @@ class Solution {
             System.out.println(list.toString());
         });
         System.out.println("nums3=====================");
-        s.threeSum(nums2).forEach(list -> {
+        s.threeSum(nums3).forEach(list -> {
             System.out.println(list.toString());
         });
     }
