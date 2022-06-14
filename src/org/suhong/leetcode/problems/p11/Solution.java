f537897c055fd5a6437b08a6c44b4168f7b6dde5
GormWH <sp4kid@gmail.com>
2022-06-14 20:35:03 +0900

    LeetCode - p11: container with most water

--- src/org/suhong/leetcode/problems/p11/Solution.java
+++ src/org/suhong/leetcode/problems/p11/Solution.java
@@ -2,9 +2,82 @@ package org.suhong.leetcode.problems.p11;
 
 // https://leetcode.com/problems/container-with-most-water/
 class Solution {
+    // brute force
     public int maxArea(int[] height) {
+        int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = height.length - 1; i < j; j--) {
+                int area = (j - i) * Math.min(height[i], height[j]);
+                if (area > maxArea) {
+                    maxArea = area;
+                }
+            }
+        }
+        return maxArea;
+    }
 
-        return 0;
+    // improved solution
+    public int maxArea2(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = (height.length - 1) * Math.min(height[left], height[right]);
+
+        outer:
+        while (left < right) {
+            System.out.printf("left: %d-%d, right: %d-%d, area: %d\n", left, height[left], right, height[right], (right - left) * Math.min(height[left], height[right]));
+            if (height[left] < height[right]) {
+                System.out.println("right is bigger");
+                for (int i = left + 1; i < right; i++) {
+                    if (height[i] > height[left]) {
+                        int area = (right - i) * Math.min(height[i], height[right]);
+                        if (maxArea < area) {
+                            maxArea = area;
+                            left = i;
+                            System.out.println("타냐 안타냐");
+                            continue outer;
+                        }
+                    }
+                }
+                break;
+            } else if (height[left] > height[right]) {
+                System.out.println("left is bigger");
+                for (int i = right - 1; left < i; i--) {
+                    if (height[i] > height[right]) {
+                        int area = (i - left) * Math.min(height[left], height[i]);
+                        if (maxArea < area) {
+                            maxArea = area;
+                            right = i;
+                            continue outer;
+                        }
+                    }
+                }
+                break;
+            } else {
+                System.out.println("left and right is same");
+                for (int i = left + 1; i < right; i++) {
+                    if (height[i] > height[left]) {
+                        if (right - i > i - left) {
+                            int area = (right - i) * Math.min(height[i], height[right]);
+                            if (maxArea < area) {
+                                maxArea = area;
+                                left = i;
+                                continue outer;
+                            }
+                        } else {
+                            int area = (i - left) * Math.min(height[left], height[i]);
+                            if (maxArea < area) {
+                                maxArea = area;
+                                right = i;
+                                continue outer;
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+        }
+        System.out.printf("left: %d-%d, right: %d-%d, area: %d\n", left, height[left], right, height[right], (right - left) * Math.min(height[left], height[right]));
+        return maxArea;
     }
 
     public static void main(String[] args) {
@@ -12,8 +85,10 @@ class Solution {
 
         int[] nums1 = {1,8,6,2,5,4,8,3,7};
         int[] nums2 = {1,1};
+        int[] nums3 = {2,3,4,5,18,17,6};
 
-        System.out.println(s.maxArea(nums1));
-        System.out.println(s.maxArea(nums2));
+        System.out.println(s.maxArea2(nums1)); // 49
+        System.out.println(s.maxArea2(nums2)); // 1
+        System.out.println(s.maxArea2(nums3)); // 17
     }
 }
