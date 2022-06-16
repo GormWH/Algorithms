a53b66d905bf40357b215a0357e1344687a13548
GormWH <sp4kid@gmail.com>
2022-06-16 21:05:46 +0900

    LeetCode - p36: is valid sudoku(solving)

--- src/org/suhong/leetcode/problems/p36/Solution.java
+++ src/org/suhong/leetcode/problems/p36/Solution.java
@@ -1,11 +1,46 @@
 package org.suhong.leetcode.problems.p36;
 
+import java.util.HashSet;
+import java.util.Set;
+
 // https://leetcode.com/problems/valid-sudoku/
 class Solution {
+    //brute force
     public boolean isValidSudoku(char[][] board) {
-
+        for (int i = 0; i < 9; i++) {
+            Set<Character> horizontal = new HashSet<>();
+            Set<Character> vertical = new HashSet<>();
+            Set<Character> square = new HashSet<>();
+            int startRow = (i / 3) * 3;
+            int startCol = (i % 3) * 3;
+            for (int j = 0; j < 9; j++) {
+                char horizontalElement = board[i][j];
+                if (isDigit(horizontalElement) && !horizontal.add(horizontalElement)) return false;
+                char verticalElement = board[j][i];
+                if (isDigit(verticalElement) && !vertical.add(verticalElement)) return false;
+                char squareElement = board[startRow + (j / 3)][startCol + j % 3];
+                if (isDigit(squareElement) && !square.add(squareElement)) return false;
+            }
+        }
         return true;
     }
+
+    private boolean isDigit(char c) {
+        return 49 <= c && c <=57; // '1' <= c <= '9'
+    }
+    /*
+    i = 0 => 0,0
+    i = 1 => 0,3
+    i = 2 => 0,6
+
+    i = 3 => 3,0
+    i = 4 => 3,3
+    i = 5 => 3,6
+
+    i = 6 => 6,0
+    i = 7 => 6,3
+    i = 8 => 6,6
+     */
 
     public static void main(String[] args) {
         Solution s = new Solution();
@@ -32,6 +67,7 @@ class Solution {
                 ,{'.','.','.','4','1','9','.','.','5'}
                 ,{'.','.','.','.','8','.','.','7','9'}
         };
+
         System.out.println(s.isValidSudoku(board1)); // true
         System.out.println(s.isValidSudoku(board2)); // false
     }
