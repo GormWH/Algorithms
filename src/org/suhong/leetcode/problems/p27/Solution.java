15b8925315c2601f318ad2c0ad70f1a62d14ec98
GormWH <sp4kid@gmail.com>
2022-06-18 23:58:28 +0900

    LeetCode - p27: remove element(init)

---
+++ src/org/suhong/leetcode/problems/p27/Solution.java
@@ -0,0 +1,12 @@
+package org.suhong.leetcode.problems.p27;
+
+public class Solution {
+
+    public int removeElement(int[] nums, int val) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
