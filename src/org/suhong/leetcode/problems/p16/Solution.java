5de64fba764fc312b8f5218787d7f8d204e57cd3
GormWH <sp4kid@gmail.com>
2022-06-24 16:30:28 +0900

    LeetCode - p16: closest three sum(solved)

---
+++ src/org/suhong/leetcode/problems/p16/Solution.java
@@ -0,0 +1,64 @@
+package org.suhong.leetcode.problems.p16;
+
+import java.util.Arrays;
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        Integer closest = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; i++) {
+            int first = nums[i];
+            for (int j = i + 1; j < nums.length-1; j++) {
+                int twoSum = first + nums[j];
+                int k = binarySearchClosest(nums, j+1, nums.length, target - twoSum);
+                int threeSum = twoSum + nums[k];
+                if (threeSum == target) return target;
+                if (Math.abs(closest - target) > Math.abs(threeSum - target)) {
+                    closest = threeSum;
+                }
+                if (k == nums.length-1 && threeSum - target > 0) break;
+            }
+        }
+
+        return closest;
+    }
+
+    private int binarySearchClosest(int[] nums, int from, int to, int target) {
+        int mid = from + (to - from) / 2;
+        int closestGap = Math.abs(nums[mid] - target);
+        int closestIdx = mid;
+        while (from < to) {
+            int currentGap = nums[mid] - target;
+            if (currentGap == 0) return mid;
+            if (Math.abs(currentGap) < closestGap) {
+                closestIdx = mid;
+                closestGap = Math.abs(currentGap);
+            }
+            if (currentGap > 0) {
+                to = mid;
+            } else {
+                from = mid + 1;
+            }
+            mid = from + (to - from) / 2;
+        }
+        return closestIdx;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] nums1 = {-1,2,1,-4};
+        int[] nums2 = {0,0,0};
+        int[] nums3 = {1,1,1,0};
+        int[] nums4 = {-55,-24,-18,-11,-7,-3,4,5,6,9,11,23,33};
+
+        System.out.println(s.threeSumClosest(nums1, 1)); // 2
+        System.out.println(s.threeSumClosest(nums2, 1)); // 0
+        System.out.println(s.threeSumClosest(nums3, 100)); // 3
+        System.out.println(s.threeSumClosest(nums4, 0)); // 0
+//        Arrays.sort(nums1);
+//        Arrays.sort(nums2);
+//        System.out.println(s.binarySearchClosest(nums1, 0, nums1.length, 0));
+//        System.out.println(s.binarySearchClosest(nums2, 0, nums2.length, -3));
+    }
+}
