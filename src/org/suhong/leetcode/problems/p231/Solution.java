85abf8345889bc173a284e3b519e0917f4a4342d
GormWH <sp4kid@gmail.com>
2022-06-27 12:11:31 +0900

    LeetCode - p231: is power of two(solved recursively)

---
+++ src/org/suhong/leetcode/problems/p231/Solution.java
@@ -0,0 +1,25 @@
+package org.suhong.leetcode.problems.p231;
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        return isPowerOfTwoRecursive(n);
+    }
+
+    private boolean isPowerOfTwoRecursive(int n) {
+        if (n % 2 == 0) {
+            return isPowerOfTwo(n / 2);
+        }
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.isPowerOfTwo(16)); // true
+        System.out.println(s.isPowerOfTwo(28)); // false
+        System.out.println(s.isPowerOfTwo(Integer.MAX_VALUE / 2 + 1)); // true
+        System.out.println(s.isPowerOfTwo(17)); // false
+        System.out.println(s.isPowerOfTwo(-1)); // false
+    }
+}
