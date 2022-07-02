469198ac0c5cfb9a4f9e31f6f8db10a83b89c01b
GormWH <sp4kid@gmail.com>
2022-07-02 19:56:10 +0900

    LeetCode - p415: addStrings(solved)

---
+++ src/org/suhong/leetcode/problems/p66/Solution.java
@@ -0,0 +1,15 @@
+package org.suhong.leetcode.problems.p66;
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] num = {1,2,3};
+
+
+    }
+}
