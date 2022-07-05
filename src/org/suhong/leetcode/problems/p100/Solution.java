27366f276c2df64ebf35f81565122f96a642759c
GormWH <sp4kid@gmail.com>
2022-07-05 23:45:27 +0900

    LeetCode - p100: same tree(just started)

---
+++ src/org/suhong/leetcode/problems/p100/Solution.java
@@ -0,0 +1,24 @@
+package org.suhong.leetcode.problems.p100;
+
+import org.suhong.leetcode.datastructure.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return true;
+    }
+}
