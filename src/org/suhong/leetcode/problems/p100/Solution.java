903fdf3fe6dbc77478f4609fd6ffec7507f21b20
GormWH <sp4kid@gmail.com>
2022-07-07 23:22:15 +0900

    LeetCode - p100: same tree(solved) using recursive method

--- src/org/suhong/leetcode/problems/p100/Solution.java
+++ src/org/suhong/leetcode/problems/p100/Solution.java
@@ -19,7 +19,11 @@ import org.suhong.leetcode.datastructure.TreeNode;
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return true;
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        if (!isSameTree(p.left, q.left)) return false;
+        return isSameTree(p.right, q.right);
     }
 
     public static void main(String[] args) {
