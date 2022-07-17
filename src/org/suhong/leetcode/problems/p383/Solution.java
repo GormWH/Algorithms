957177d7729d968dec3dafbd28f18eed6f659433
GormWH <sp4kid@gmail.com>
2022-07-17 15:50:17 +0900

    LeetCode - p383: Random Note(solved!) - improved with hashing

--- src/org/suhong/leetcode/problems/p383/Solution.java
+++ src/org/suhong/leetcode/problems/p383/Solution.java
@@ -1,9 +1,12 @@
 package org.suhong.leetcode.problems.p383;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // https://leetcode.com/problems/ransom-note/
 class Solution {
     // brute force
-    public boolean canConstruct(String ransomNote, String magazine) {
+    public boolean canConstruct1(String ransomNote, String magazine) {
         StringBuilder magazineManager = new StringBuilder(magazine);
         outer:
         for (char chr: ransomNote.toCharArray()) {
@@ -14,6 +17,19 @@ class Solution {
                 }
             }
             return false;
+        }
+        return true;
+    }
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabetCounts = new int[26];
+        for (char alphabet: magazine.toCharArray()) {
+            alphabetCounts[alphabet - 'a']++;
+        }
+        for (char alphabet: ransomNote.toCharArray()) {
+            if (alphabetCounts[alphabet - 'a'] == 0) return false;
+            alphabetCounts[alphabet - 'a']--;
         }
         return true;
     }
