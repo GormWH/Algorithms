0fa4bd5f93b45ccb25c5528762f80b3adc1ec11f
GormWH <sp4kid@gmail.com>
2022-07-21 15:46:55 +0900

    LeetCode - explore arrays101: Squares of a Sorted Array

--- src/org/suhong/leetcode/explore/arrays101/_1_introduction/SquaresOfASortedArray/Solution.java
+++ src/org/suhong/leetcode/explore/arrays101/_1_introduction/SquaresOfASortedArray/Solution.java
@@ -1,6 +1,8 @@
-package org.suhong.leetcode.explore.arrays101.squares_of_a_sorted_array;
+package org.suhong.leetcode.explore.arrays101._1_introduction.SquaresOfASortedArray;
 
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
     public int[] sortedSquares(int[] nums) {
         int size = nums.length;
         if (size == 1) return new int[]{nums[0] * nums[0]};
@@ -25,7 +27,9 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution s = new Solution();
 
+        System.out.println(Arrays.toString(s.sortedSquares(new int[]{-4,-1,0,3,10}))); // 0,1,9,16,100
+        System.out.println(Arrays.toString(s.sortedSquares(new int[]{-7,-3,2,3,11}))); // 4,9,9,49,121
     }
 }
