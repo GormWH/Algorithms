5329aa50f158724faec184d40dcd77f9a23b28c3
GormWH <sp4kid@gmail.com>
2022-08-01 07:58:07 +0900

    LeetCode - explore arrays101: move zeros

--- src/org/suhong/leetcode/explore/arrays101/_5_inplace/move_zeroes/Solution.java
+++ src/org/suhong/leetcode/explore/arrays101/_5_inplace/move_zeroes/Solution.java
@@ -3,29 +3,24 @@ package org.suhong.leetcode.explore.arrays101._5_inplace.move_zeroes;
 import java.util.Arrays;
 
 // 2022.03.30
-public class Solution1 {
+public class Solution {
     public void moveZeroes(int[] nums) {
         int length = nums.length;
-        int indexOfFirstZero = -1;
-        int i = 0;
-        for (; i < length; i++) {
-            if (nums[i] == 0) {
-                indexOfFirstZero = i++;
-                break;
+        int writer = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (nums[i] != 0) {
+                nums[writer++] = nums[i];
             }
         }
-        for (; i < length; i++) {
-
-            if (nums[i] != 0) {
-                nums[indexOfFirstZero++] = nums[i];
-                nums[i] = 0;
-            }
+        for (;writer < length; writer++) {
+            nums[writer] = 0;
         }
 
     }
 
     public static void main(String[] args) {
-        Solution1 solution1 = new Solution1();
+        Solution solution1 = new Solution();
 
         int[] nums1 = {0,1,0,3,12};
         solution1.moveZeroes(nums1);
