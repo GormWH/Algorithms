a7c211df3c0943ba3d7c71c687a7cb3ca82a4847
GormWH <sp4kid@gmail.com>
2022-08-08 23:52:42 +0900

    LeetCode -Linked List: detect cycle(solved)

---
+++ src/org/suhong/leetcode/explore/linkedlist/_2_two_pointer_techique/linked_list_cycle2/Solution.java
@@ -0,0 +1,26 @@
+package org.suhong.leetcode.explore.linkedlist._2_two_pointer_techique.linked_list_cycle2;
+
+import org.suhong.leetcode.datastructure.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        int oneTwo = 0;
+        int oneThree = 0;
+        ListNode one = head;
+        ListNode Two = head;
+        ListNode Three = head;
+
+    }
+}
