6296c5040bfbf7a78b45ee87175ad63b364fe5d6
GormWH <sp4kid@gmail.com>
2022-08-12 23:59:15 +0900

    LeetCode -Linked List: Intersection of Two Linked Lists(solving...)

--- src/org/suhong/leetcode/explore/linkedlist/_2_two_pointer_techique/intersection_two_linked_lists/Solution.java
+++ src/org/suhong/leetcode/explore/linkedlist/_2_two_pointer_techique/intersection_two_linked_lists/Solution.java
@@ -1,4 +1,8 @@
 package org.suhong.leetcode.explore.linkedlist._2_two_pointer_techique.intersection_two_linked_lists;
 
 public class Solution {
+
+    public static void main(String[] args) {
+        //
+    }
 }
