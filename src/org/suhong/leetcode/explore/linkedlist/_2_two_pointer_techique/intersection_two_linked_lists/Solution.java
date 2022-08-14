d0deb1320bde416a6ca4a82c5b3d6a18e25cf53f
GormWH <sp4kid@gmail.com>
2022-08-15 07:50:26 +0900

    LeetCode -Linked List: Intersection of Two Linked Lists(solved)

--- src/org/suhong/leetcode/explore/linkedlist/_2_two_pointer_techique/intersection_two_linked_lists/Solution.java
+++ src/org/suhong/leetcode/explore/linkedlist/_2_two_pointer_techique/intersection_two_linked_lists/Solution.java
@@ -5,7 +5,46 @@ import org.suhong.leetcode.datastructure.ListNode;
 public class Solution {
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lengthA = 1;
+        ListNode pointerA = headA;
+        while (pointerA.next != null) {
+            pointerA = pointerA.next;
+            lengthA++;
+        }
 
+
+        int lengthB = 1;
+        ListNode pointerB = headB;
+        while (pointerB.next != null) {
+            pointerB = pointerB.next;
+            lengthB++;
+        }
+
+        if (pointerA != pointerB) {
+            return null; // there is no intersection node
+        }
+
+        pointerA = headA;
+        pointerB = headB;
+
+        int gap = lengthA - lengthB;
+        if (lengthA < lengthB) {
+            gap = -gap;
+            for (int i = 0; i < gap; i++) {
+                pointerB = pointerB.next;
+            }
+        } else {
+            for (int i = 0; i < gap; i++) {
+                pointerA = pointerA.next;
+            }
+        }
+
+        ListNode result = null;
+        while (pointerA!= null && pointerA != pointerB) {
+            pointerA = pointerA.next;
+            pointerB = pointerB.next;
+        }
+        return pointerA;
     }
 
     public static void main(String[] args) {
