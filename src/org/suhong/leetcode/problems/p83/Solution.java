5509dd81ee77464e501ba827c9aa4c294dc85382
GormWH <sp4kid@gmail.com>
2022-08-29 22:57:13 +0900

    LeetCode p83 remove duplicates from sorted list

---
+++ src/org/suhong/leetcode/problems/p83/Solution.java
@@ -0,0 +1,21 @@
+package org.suhong.leetcode.problems.p83;
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+import org.suhong.leetcode.datastructure.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        return null;
+    }
+}
